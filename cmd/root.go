b6a9841bfa33783131953940c19e1129acfb44ea
samueltobyknight <56262589+samueltobyknight@users.noreply.github.com>
2022-12-04 11:32:49 +0000

    use flags instead of arguments for line declaration

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package xpln
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	xpln "github.com/5amCurfew/xpln/pkg"
 	"github.com/spf13/cobra"
@@ -11,20 +12,20 @@ import (
 var version = "0.0.1"
 
 var rootCmd = &cobra.Command{
-	Use:     "xpln",
+	Use:     "xpln <PATH_TO_FILE>",
 	Version: version,
 	Short:   "xpln - a CLI to explain code blocks",
 	Long:    `xpln is a command line interface to inspect and explain code blocks using OpenAI's GPT-3 (refer to beta.openai.com). `,
-	Args:    cobra.MaximumNArgs(3),
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-
 		var file = args[0]
 		var start, end string
-		if len(args) == 2 {
-			start = args[1]
-		} else if len(args) == 3 {
-			start = args[1]
-			end = args[2]
+		lineRange, _ := cmd.Flags().GetString("lines")
+
+		// Check for lines flag provided
+		if lineRange != "" {
+			start = strings.Split(lineRange, "-")[0]
+			end = strings.Split(lineRange, "-")[1]
 		}
 
 		var block = xpln.ReadCodeBlock(file, start, end)
@@ -33,6 +34,8 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	rootCmd.PersistentFlags().StringP("lines", "l", "", "Declare the lines of the code block (start-end e.g. 5-10)")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error using xpln: '%s'", err)
 		os.Exit(1)
